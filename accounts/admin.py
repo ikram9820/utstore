ec5b70627e6416dac6cd2f025624ba775d06b8fd
ikram9820 <ikram9820khan@gmail.com>
2023-05-16 16:49:46 +0500

    implementing admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,25 @@
+from store.models import Product
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from store.admin import ProductAdmin, ProductImageInline
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'email', 'first_name', 'last_name'),
+        }),
+    )
+
+
+
+
+class CustomProductAdmin(ProductAdmin):
+    inlines = [ ProductImageInline]
+
+
+admin.site.unregister(Product)
+admin.site.register(Product, CustomProductAdmin)

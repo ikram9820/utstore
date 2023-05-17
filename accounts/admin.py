e7f32065148eb517180829722053a35bbf1637b1
ikram9820 <ikram9820khan@gmail.com>
2023-05-17 13:53:28 +0500

    adding basic templates and sttings for user authentication

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,15 +6,19 @@ from store.admin import ProductAdmin, ProductImageInline
 from tags.models import TaggedItem
 from .models import User
 
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'password1', 'password2', 'email', 'first_name', 'last_name'),
-        }),
-    )
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = [
+        "email",
+        "username",
+        "is_superuser",
+    ]
 
 
 class TagInline(GenericTabularInline):
@@ -28,3 +32,5 @@ class CustomProductAdmin(ProductAdmin):
 
 admin.site.unregister(Product)
 admin.site.register(Product, CustomProductAdmin)
+
+
